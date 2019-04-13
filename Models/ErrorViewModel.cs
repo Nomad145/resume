d1a2747a08f5d91b9f43ce9699eee2fabbc62a14
Michael Phillips <michaeljoelphillips@gmail.com>
2019-04-12 23:43:39 -0500

    Initial commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace michaelphillips.dev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
